0a907f203115ebc591ddba2b85c0ffa8b07d7e49
Nikita Golubev <nekitgo80@gmail.com>
2024-08-26 20:18:29 +0300

    new

--- spec/factories/companies.rb
+++
@@ -1,4 +0,0 @@
-FactoryBot.define do
-  factory :company do
-  end
-end
--- spec/factories/tickets.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :ticket do
-    title { Faker::Lorem.characters(number: 10) }
-    body { Faker::Lorem.paragraphs(number: 1) }
-    company
-    creator { create(:user) }
-  end
-end
