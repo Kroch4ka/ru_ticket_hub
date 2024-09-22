be09240ef0baf9bd9378e6be9b60383cbfd7cdcc
Kroch4ka <64526981+Kroch4ka@users.noreply.github.com>
2024-09-22 19:22:51 +0300

    refactor api (#15)

---
+++ app/api/v1/tickets.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+module V1
+  class Tickets < Grape::API
+    format :json
+
+    include Grape::Jwt::Authentication
+
+    helpers ::Helpers::AuthHelpers
+
+    auth :jwt
+
+    resource :tickets do
+      params do
+        requires :subject, type: String
+        requires :body, type: String
+        requires :company_id, type: Integer
+      end
+      post do
+        declared_params = declared(params)
+        result = CreateTicket.call(**declared_params, profile: current_account.profile)
+        error! result.message, status: 422 unless result.success?
+      end
+
+      params do
+        requires :id, type: Integer
+      end
+      post ':id/take' do
+        declared_params = declared(params)
+        result = AssignTicket.call(**declared_params, operator_id: current_account.profile.operator.id)
+        error! result.message, status: 422 unless result.success?
+      end
+
+      params do
+        requires :id, type: Integer
+        requires :operator_id, type: Integer
+      end
+      post ':id/assign' do
+        declared_params = declared(params)
+        result = AssignTicket.call(**declared_params)
+        error! result.message, status: 422 unless result.success?
+      end
+
+      params do
+        requires :id, type: Integer
+      end
+      post ':id/cancel' do
+        declared_params = declared(params)
+        result = CancelTicket.call(**declared_params)
+        error! result.message, status: 422 unless result.success?
+      end
+
+      params do
+        requires :id, type: Integer
+        requires :body, type: String
+      end
+      post ':id/reply' do
+        declared_params = declared(params)
+        result = ReplyToTicket.call(**declared_params, operator_id: current_account.profile.operator.id)
+        error! result.message, status: 422 unless result.success?
+      end
+    end
+  end
+end
--- db/migrate/20240914184007_create_companies.rb
+++ db/migrate/20240914184007_create_companies.rb
@@ -4,6 +4,11 @@ class CreateCompanies < ActiveRecord::Migration[7.1]
   def change
     create_table :companies do |t|
       t.string :name, null: false
+      t.text :description
+      t.string :email
+      t.string :website
+      t.string :inn
+      t.boolean :active, default: false, null: false
       t.timestamps
     end
   end
